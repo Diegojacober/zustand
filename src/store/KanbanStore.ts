0e09b98044e3a62e7bb976e55e169ea854ced4a6
Diego <diegoalencar.jacober@gmail.ocm>
2023-11-08 19:03:06 -0300

    Middlewares

--- src/store/KanbanStore.ts
+++ src/store/KanbanStore.ts
@@ -1,16 +1,29 @@
 import { create } from "zustand";
-import { Status } from "../enums/Status";
+import { persist } from "zustand/middleware";
 import Store from "../types/Store";
 
-export const useKanbanStore = create<Store>((set) => {
-  return {
-    tasks: [{  title: "TestTask", status: Status.PLANNED }],
-    draggedTask: null,
-    addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
-    deleteTask: (title) =>
-      set((state) => ({ tasks: state.tasks.filter((task) => task.title !== title) })),
-    setDraggedTask: (task) => set({ draggedTask: task }),
-    moveTask: (title, status) => set((store) => ({ tasks: store.tasks.map((task) => task.title === title ? { title, status} : task),
-      })),
-  };
-});
+export const useKanbanStore = create(
+  persist<Store>(
+    (set) => {
+      return {
+        tasks: [],
+        draggedTask: null,
+        addTask: (task) => set((store) => ({ tasks: [...store.tasks, task] })),
+        deleteTask: (title) =>
+          set((store) => ({
+            tasks: store.tasks.filter((task) => task.title !== title),
+          })),
+        setDraggedTask: (task) => set({ draggedTask: task }),
+        moveTask: (title, status) =>
+          set((store) => ({
+            tasks: store.tasks.map((task) =>
+              task.title === title ? { title, status } : task
+            ),
+          })),
+      };
+    },
+    {
+      name: "store",
+    }
+  )
+);
